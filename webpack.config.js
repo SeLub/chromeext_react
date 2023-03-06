9bdd80de2f0a6505147b1a6da7bc9da78cb450c6
Sergey Lubimov <siarhei_liubimau@epam.com>
2023-03-06 16:49:45 +0300

    init: start project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+
+export default {
+      entry: {
+            popup: './src/popup.js',
+      },
+      output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+          },
+          module: {
+            rules: [{ 
+                        test: /\.(js)$/,
+                        exclude: /node_modules/, 
+                        use: {
+                              loader: 'babel-loader',
+                              options: {
+                                    presets: ['@babel/preset-env', '@babel/preset-react']
+                              }
+                        }
+                  },
+            ],
+          },
+          plugins: [
+            new HtmlWebpackPlugin({
+            template: './src/popup.html',
+            filename: 'popup.html'
+          }),
+            new CopyPlugin({
+                  patterns: [
+                        { from: "public" },
+                  ]}),
+          ],
+    };
